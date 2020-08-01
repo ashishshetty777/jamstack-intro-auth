43e521919709bf9f21ca82fd6d1ab97b5918c283
Ashish Shetty <ashishshetty777@gmail.com>
2020-08-02 02:16:56 +0530

    JAMstack auth

---
+++ src/components/route-base.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RouteBase = () => {
+    return (
+        <h1>All your base belongs here!!!</h1>
+    )
+}
+
+export default RouteBase;
---
+++ src/components/route-login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RouteLogin= () => {
+    return (
+        <>
+            <h1>Login or Signup</h1>
+            <button>Login</button>
+        </>
+    )
+}
+
+export default RouteLogin;
